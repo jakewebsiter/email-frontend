04d7dea6f6fe3716060fe3ee03c3b35434d94540
Jake Harris <jakeharris30@outlook.com>
2024-05-12 12:57:26 +0100

    working?

--- scripts.js
+++ scripts.js
@@ -36,6 +36,7 @@ async function sendEmails(num) {
       headers: { 'ngrok-skip-browser-warning': 0 },
     }
   );
+  console.log(await res);
   resData = await res.json();
   return resData.message;
 }
