0767a4e072fe11b66fd50183c68e2d55f9b88bc0
Jake Harris <jakeharris30@outlook.com>
2024-05-12 13:48:30 +0100

    working?

--- scripts.js
+++ scripts.js
@@ -59,6 +59,6 @@ async function sendEmails(num) {
 // Stop the interval after a certain time (optional)
 setTimeout(async () => {
   clearInterval(intervalId);
-  message.innerHTML = '...';
+  message.innerHTML = 'pending...';
   sendEmails(parseInt(numberElement.innerText));
 }, 5000); // Stop after 10 seconds (10000 milliseconds)
